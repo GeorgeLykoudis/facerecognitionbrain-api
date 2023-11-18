ef37306a518e40410c2c3067614ecdf0a9c6c948
George Lykoudis <glykoudis92@gmail.com>
2023-11-18 20:25:49 +0200

    load user feature

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get('/', (req, res) => {
 app.post('/signin', (req, res) => {
     if (req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password) {
-        res.json('success');
+        res.json(database.users[0]);
     } else {
         res.status(404).json('error signing in');
     }
